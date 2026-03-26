b09219310e6611845b4ca2a23075f8056dc19145
vadukomacha <vadukomacha9@gmail.com>
2026-03-27 00:06:48 +0530

    final change1

---
+++ week11/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def contactUs(request):
+    return render(request, 'contactus.html')
