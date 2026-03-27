6ece26d120f479fcbf7743f47a65f72066350aaf
vadukomacha <vadukomacha9@gmail.com>
2026-03-27 23:01:29 +0530

    Create index.js

---
+++ week10/src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom/client";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+ import Layouts from "./templates/Layouts";
+ import Home from "./templates/Home";
+ import Profile from "./templates/Profile";
+ import Contactus from "./templates/Contactus";
+ export default function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layouts/>}>
+      <Route index element={<Home/>}/>
+      <Route path="profile" element={<Profile/>}/>
+      <Route path="contact" element={<Contactus/>}/>      
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+ }
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>)
